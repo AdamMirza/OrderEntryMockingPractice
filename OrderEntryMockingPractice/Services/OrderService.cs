264b4ebfe504cf549faa09f647759ea84f58612e
PARAPORT\AdamM <amirza@paraport.com>
2017-05-12 15:45:59 -0700

    Wrote and implemented logic for tests to check if an order is valid in the OrderService.

--- OrderEntryMockingPractice/Services/OrderService.cs
+++ OrderEntryMockingPractice/Services/OrderService.cs
@@ -1,12 +1,44 @@
-﻿using OrderEntryMockingPractice.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using OrderEntryMockingPractice.Models;
 
 namespace OrderEntryMockingPractice.Services
 {
     public class OrderService
     {
+        private readonly IProductRepository _productRepository;
+
+        public OrderService(IProductRepository productRepository)
+        {
+            this._productRepository = productRepository;
+        }
+
         public OrderSummary PlaceOrder(Order order)
         {
+            CheckIfOrderIsValid(order);
+
             return null;
+        }
+
+        private void CheckIfOrderIsValid(Order order)
+        {
+            List<string> productSkus = new List<string>();
+
+            foreach (var item in order.OrderItems)
+            {
+                if (productSkus.Contains(item.Product.Sku))
+                {
+                    throw new Exception("Order Items are not unique by Product SKU.");
+                }
+
+                if (!_productRepository.IsInStock(item.Product.Sku))
+                {
+                    throw new Exception("One or more products are out of stock.");
+                }
+
+                productSkus.Add(item.Product.Sku);
+            }
         }
     }
 }
--- OrderEntryMockingPracticeTests/OrderServiceTests.cs
+++ OrderEntryMockingPracticeTests/OrderServiceTests.cs
@@ -1,6 +1,81 @@
-﻿namespace OrderEntryMockingPracticeTests
+﻿using System;
+using NUnit.Framework;
+using OrderEntryMockingPractice.Models;
+using OrderEntryMockingPractice.Services;
+using Rhino.Mocks;
+
+namespace OrderEntryMockingPracticeTests
 {
+    [TestFixture]
     public class OrderServiceTests
     {
+        private IOrderFulfillmentService _mockOrderFulfillmentService = MockRepository.GenerateMock<IOrderFulfillmentService>();
+
+        [SetUp]
+        public void SetUp()
+        {
+            
+        }
+
+        [Test]
+        public void PlaceOrder_NonUniqueProductSkus_ExceptionThrownWithInfo()
+        {
+            // Arrange
+            var mockProductRepository = MockRepository.GenerateMock<IProductRepository>();
+            mockProductRepository.Stub(p => p.IsInStock("1")).Return(true);
+
+            var orderService = new OrderService(mockProductRepository);
+            var order = new Order();
+            var orderItem1 = new OrderItem
+            {
+                Product = new Product
+                {
+                    Sku = "1"
+                }
+            };
+
+            var orderItem2 = new OrderItem
+            {
+                Product = new Product
+                {
+                    Sku = "1"
+                }
+            };
+
+            order.OrderItems.Add(orderItem1);
+            order.OrderItems.Add(orderItem2);
+            
+            // Act
+            var exceptionMessage = Assert.Throws<Exception>(() => orderService.PlaceOrder(order));
+
+            // Assert
+            Assert.That(exceptionMessage.Message, Is.EqualTo("Order Items are not unique by Product SKU."));
+        }
+
+        [Test]
+        public void PlaceOrder_ProductNotInStock_ExceptionThrownWithInfo()
+        {
+            var mockProductRepository = MockRepository.GenerateMock<IProductRepository>();
+            mockProductRepository.Stub(p => p.IsInStock("1")).Return(false);
+
+            // Arrange
+            var orderService = new OrderService(mockProductRepository);
+            var order = new Order();
+            var orderItem = new OrderItem
+            {
+                Product = new Product
+                {
+                    Sku = "1"
+                }
+            };
+
+            order.OrderItems.Add(orderItem);
+
+            // Act
+            var exceptionMessage = Assert.Throws<Exception>(() => orderService.PlaceOrder(order));
+
+            // Assert
+            Assert.That(exceptionMessage.Message, Is.EqualTo("One or more products are out of stock."));
+        }
     }
 }
